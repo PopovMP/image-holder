855d576a3f90904d9ed241e8dfe492ea0e3de0b2
Miroslav Popov <info@DESKTOP-E6RTRT6>
2018-07-03 11:33:11 +0300

    Code cleanup.

--- file-upload.js
+++ file-upload.js
@@ -28,33 +28,39 @@ function uploadDataFile(req, res) {
         return;
     }
 
-    const filePath = path.join(__dirname, "public", settings.storagePath, fileName);
-
-    console.log(filePath);
-
-    const host = encodeURIComponent(req.headers.origin);
-    const fileUrl = path.join(host, settings.storagePath, fileName);
-
     let content = "";
     req.on("data", chunk => content += chunk);
     req.on("end", req_end);
 
     function req_end() {
         if (content) {
-            const data = content.replace(/^data:image\/\w+;base64,/, "");
-            const buffer = new Buffer(data, "base64");
-            fs.writeFile(filePath, buffer, fs_writeFile_ready);
+            saveFile(content);
         } else {
             res.json({err: "Error with receiving content!", data: null});
         }
+    }
+
+    function saveFile(content) {
+        const data = content.replace(/^data:image\/\w+;base64,/, "");
+        const buffer = new Buffer(data, "base64");
+        const filePath = path.join(__dirname, "public", settings.storagePath, fileName);
+
+        fs.writeFile(filePath, buffer, fs_writeFile_ready);
     }
 
     function fs_writeFile_ready(err) {
         if (err) {
             res.json({err: err, data: null});
         } else {
-            res.json({err: null, data: fileUrl});
+            sendResponse();
         }
+    }
+
+    function sendResponse() {
+        const host = encodeURIComponent(req.headers.origin);
+        const fileUrl = path.join(host, settings.storagePath, fileName);
+
+        res.json({err: null, data: fileUrl});
     }
 }
 
